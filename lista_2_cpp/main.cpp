592982da62378d1438e6c11e2cf5e78f2564b163
Mateusz Sierakowski <mateuszsierakowski.rudy@gmail.com>
2024-10-23 21:55:44 +0200

    proba zrobienia pierwszego zadania lista 2

--- lista_2_cpp/main.cpp
+++ lista_2_cpp/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <cctype>
 
-//template<typename T>
-void insertion_sort(std::vector<std::string> &vec){
-    for(size_t i=1;i<vec.size();++i){
-        std::string key=vec[i];
+// insertion sort domyslna
+template <typename T>
+void insertion_sort(std::vector<T> &vec){
+    for(size_t i=1; i<vec.size();++i){
+        T key=vec[i];
         int j=i-1;
-        while(j>=0 && vec[j].length() > key.length()){
+        while(j>=0 && vec[j]>key){
             vec[j+1]=vec[j];
             --j;
         }
@@ -14,9 +16,27 @@ void insertion_sort(std::vector<std::string> &vec){
     }
 }
 
+std::vector<int> insertion_str(std::vector<std::string> &str){
+    std::string numberstr="";
+    std::vector<int> result;
+    for(size_t i=1;i<str.size();++i){
+        for(char ch:str[i]){
+            if(std::isdigit(ch))
+                numberstr += ch;
+        }
+    }
+
+
+    return result;
+}
+
+
+
+
+
 int main(){
     std::vector<std::string> one={"zadanie 1", "zadanie 2", "zadanie 11", "zadanie 22", "zadanie 3"};
-    insertion_sort(one);
+    insertion_str(one);
     for(const auto &elem:one)
         std::cout<<elem<<" ";
     std::cout<<"\n";
